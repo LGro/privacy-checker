e5f3bcac07c9f8bfc5a72840a89940bc0cab2ca2
Lukas Großberger <lukas.grossberger@stud.tu-darmstadt.de>
2014-06-11 15:07:02 +0200

    fixed open issues after review

--- src/de/otaris/zertapps/privacychecker/database/App.java
+++ src/de/otaris/zertapps/privacychecker/database/App.java
@@ -3,50 +3,73 @@ package de.otaris.zertapps.privacychecker.database;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+/**
+ * represents the App entity in the database
+ * 
+ * Attention: When adding a new column, mind adding it in the matching
+ * DataSource's cursorTo... method.
+ */
 public class App {
 
-	// Database table
+	// table name
 	public static final String TABLE = "tbl_app";
 
-	// Columns
+	// table columns
 	public static final String ID = "app_id";
 	public static final String NAME = "name";
+	public static final String LABEL = "label";
 	public static final String VERSION = "version";
 	public static final String RATING = "rating";
 	public static final String INSTALLED = "installed";
 
-	// Creation statement
+	// SQL statement to create table
 	private static final String Create_App_Table = "CREATE TABLE " + TABLE
 			+ "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME
-			+ " TEXT, " + VERSION + " TEXT, " + RATING + " FLOAT, " + INSTALLED
-			+ " INT);";
+			+ " TEXT UNIQUE ON CONFLICT REPLACE, " + LABEL + " TEXT, "
+			+ VERSION + " TEXT, " + RATING + " FLOAT, " + INSTALLED + " INT);";
 
-	// params
-	int id;
-	String name;
-	String version;
-	float rating;
-	boolean installed;
+	// attributes
+	private int id;
+	private String name;
+	private String label;
+	private String version;
+	private float rating;
+	private boolean installed;
 
-	// empty constructor
 	public App() {
 
 	}
 
-	// Constructor
-	public App(String name, String version, float rating, boolean installed) {
+	public App(String name, String label, String version, float rating,
+			boolean installed) {
 		this.name = name;
+		this.label = label;
 		this.version = version;
 		this.rating = rating;
 		this.installed = installed;
 	}
 
-	// create table if it isn't existing yet
+	/**
+	 * create table if it isn't existing yet
+	 * 
+	 * @param db
+	 *            Database to create the table in
+	 */
 	public static void onCreate(SQLiteDatabase db) {
 		db.execSQL(Create_App_Table);
 	}
 
-	// upgrade Method
+	/**
+	 * upgrade an older version of this table currently drops the old table and
+	 * calls onCreate
+	 * 
+	 * @param db
+	 *            Database to update
+	 * @param oldVersion
+	 *            old database version number
+	 * @param newVersion
+	 *            new database version number
+	 */
 	public static void onUpgrade(SQLiteDatabase db, int oldVersion,
 			int newVersion) {
 		Log.w(App.class.getCanonicalName(), "upgrading database from version "
@@ -71,6 +94,10 @@ public class App {
 		this.name = name;
 	}
 
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
 	public void setInstalled(boolean installed) {
 		this.installed = installed;
 	}
@@ -90,6 +117,10 @@ public class App {
 
 	public String getName() {
 		return name;
+	}
+
+	public String getLabel() {
+		return label;
 	}
 
 	public float getRating() {
--- src/de/otaris/zertapps/privacychecker/database/AppDataSource.java
+++ src/de/otaris/zertapps/privacychecker/database/AppDataSource.java
@@ -9,11 +9,14 @@ import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
+/**
+ * Handles requests concerning Apps to the database.
+ */
 public class AppDataSource {
 
 	private SQLiteDatabase database;
 	private DatabaseHelper dbHelper;
-	private String[] allColumns = { App.ID, App.NAME, App.VERSION,
+	private String[] allColumns = { App.ID, App.NAME, App.LABEL, App.VERSION,
 			App.INSTALLED, App.RATING };
 
 	public AppDataSource(Context context) {
@@ -37,7 +40,7 @@ public class AppDataSource {
 	}
 
 	/**
-	 * convert database result cursor to app object
+	 * convert database result cursor (pointing to a result set) to app object
 	 * 
 	 * @param cursor
 	 * @return cursor data as App object
@@ -47,28 +50,31 @@ public class AppDataSource {
 
 		app.setId(cursor.getInt(0));
 		app.setName(cursor.getString(1));
-		app.setVersion(cursor.getString(2));
+		app.setLabel(cursor.getString(2));
+		app.setVersion(cursor.getString(3));
 		// convert integer from SQLite to boolean in model representation
-		app.setInstalled(cursor.getInt(3) != 0);
-		app.setRating(cursor.getFloat(4));
+		app.setInstalled(cursor.getInt(4) != 0);
+		app.setRating(cursor.getFloat(5));
 
 		return app;
 	}
 
 	/**
-	 * create app in DB by name
-	 * 
-	 * TODO: check duplicates
+	 * create app in DB by all attributes
 	 * 
 	 * @param name
-	 *            name of the App
-	 * @return newly created App with ID
+	 * @param label
+	 * @param version
+	 * @param installed
+	 * @param rating
+	 * @return app object of the newly created app
 	 */
-	public App createApp(String name, String version, boolean installed,
-			float rating) {
+	public App createApp(String name, String label, String version,
+			boolean installed, float rating) {
 		// set values for columns
 		ContentValues values = new ContentValues();
 		values.put(App.NAME, name);
+		values.put(App.LABEL, label);
 		values.put(App.VERSION, version);
 		values.put(App.INSTALLED, installed);
 		values.put(App.RATING, rating);
@@ -77,28 +83,67 @@ public class AppDataSource {
 		long insertId = database.insert(App.TABLE, null, values);
 
 		// get recently inserted App by ID
+		return getAppById(insertId);
+	}
+
+	/**
+	 * gets single app by id from database
+	 * 
+	 * @param appId
+	 *            id to identify a single app
+	 * @return app object for given id
+	 */
+	public App getAppById(long appId) {
+		// build database query
 		Cursor cursor = database.query(App.TABLE, allColumns, App.ID + " = "
-				+ insertId, null, null, null, null);
+				+ appId, null, null, null, null);
 		cursor.moveToFirst();
 
 		// convert to App object
 		App newApp = cursorToApp(cursor);
 		cursor.close();
 
-		// return newly inserted App
+		// return app object
 		return newApp;
 	}
 
 	/**
-	 * get all Apps from the DB
+	 * get all Apps from the DB, ordered by privacy rating (high to low) and
+	 * label (alphabetically)
 	 * 
 	 * @return list of all Apps
 	 */
 	public List<App> getAllApps() {
 		List<App> apps = new ArrayList<App>();
 
+		String orderBy = App.RATING + " DESC, " + App.LABEL + " ASC";
 		Cursor cursor = database.query(App.TABLE, allColumns, null, null, null,
-				null, null);
+				null, orderBy);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			App app = cursorToApp(cursor);
+			apps.add(app);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return apps;
+	}
+
+	/**
+	 * get all Apps from the DB that are marked as installed, ordered by privacy
+	 * rating (high to low) and label (alphabetically)
+	 * 
+	 * @return sorted list of all locally installed Apps
+	 */
+	public List<App> getInstalledApps() {
+		List<App> apps = new ArrayList<App>();
+
+		// build query
+		String whereClause = App.INSTALLED + " = 1";
+		String orderBy = App.RATING + " DESC, " + App.LABEL + " ASC";
+		Cursor cursor = database.query(App.TABLE, allColumns, whereClause,
+				null, null, null, orderBy);
+
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
 			App app = cursorToApp(cursor);
