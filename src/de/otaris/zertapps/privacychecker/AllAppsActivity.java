554d2ff37695ee659297a5d84495ccdd393ca91e
Nicolina <n.wenzler@gmx.de>
2014-07-07 18:21:31 +0200

    Fixed one error

--- src/de/otaris/zertapps/privacychecker/AllAppsActivity.java
+++ src/de/otaris/zertapps/privacychecker/AllAppsActivity.java
@@ -65,7 +65,7 @@ public class AllAppsActivity extends SortableAppListActivity implements
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.installed_apps, menu);
+		getMenuInflater().inflate(R.menu.all_apps, menu);
 		return true;
 	}
 
