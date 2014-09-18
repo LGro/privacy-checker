72e27e02b34f7b920913a78b60d56a2a78fe55a3
Nicolina <n.wenzler@gmx.de>
2014-09-18 13:31:42 +0200

    added rate app dont show whitespace when there are no permissions demanded

--- src/de/otaris/zertapps/privacychecker/appDetails/rateApp/permissionsExpected/PermissionsExpectedViewHelper.java
+++ src/de/otaris/zertapps/privacychecker/appDetails/rateApp/permissionsExpected/PermissionsExpectedViewHelper.java
@@ -10,6 +10,8 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 import de.otaris.zertapps.privacychecker.PrivacyCheckerAlert;
 import de.otaris.zertapps.privacychecker.R;
@@ -23,6 +25,33 @@ import de.otaris.zertapps.privacychecker.database.model.Permission;
  * the user what to do list of permission + checkbox
  */
 public class PermissionsExpectedViewHelper extends RatingElementViewHelper {
+
+	protected TextView explanation;
+	protected ListView permissionsList;
+	protected ToggleButton showMoreButton;
+	protected RelativeLayout permissionsGroup;
+
+	/**
+	 * initialize all relevant views
+	 * 
+	 * @param contextView
+	 *            view that wraps the relevant subviews
+	 */
+	protected void initializeViews(View contextView) {
+
+		explanation = (TextView) contextView
+				.findViewById(R.id.app_detail_rate_app_permissions_explanation);
+
+		permissionsList = (ListView) contextView
+				.findViewById(R.id.app_detail_rate_app_permissions_list);
+
+		showMoreButton = (ToggleButton) contextView
+				.findViewById(R.id.app_detail_rate_app_permissions_more);
+
+		permissionsGroup = (RelativeLayout) contextView
+				.findViewById(R.id.app_detail_rate_app_permissions_group);
+
+	}
 
 	@Override
 	public View getView(Context context, ViewGroup parent,
@@ -38,64 +67,71 @@ public class PermissionsExpectedViewHelper extends RatingElementViewHelper {
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View rowView = inflater.inflate(
 				R.layout.app_detail_rate_app_permissions, parent, false);
+		
+		initializeViews(rowView);
 
-		ListView permissionsList = (ListView) rowView
-				.findViewById(R.id.app_detail_rate_app_permissions_list);
+		// dont show permissionsGroup if there are no permissions
+		if (app.getPermissionList().isEmpty()) {
+			permissionsGroup.setVisibility(ViewGroup.GONE);
+			explanation
+					.setText(R.string.app_details_privacy_rating_permissions_title_no_permissions);
 
-		ToggleButton showMoreButton = (ToggleButton) rowView
-				.findViewById(R.id.app_detail_rate_app_permissions_more);
+		} else {
 
-		// set adapter
-		PermissionsExpectedItemAdapter adapter = new PermissionsExpectedItemAdapter(
-				context, app.getPermissionList());
-		permissionsList.setAdapter(adapter);
+			// set adapter
+			PermissionsExpectedItemAdapter adapter = new PermissionsExpectedItemAdapter(
+					context, app.getPermissionList());
+			permissionsList.setAdapter(adapter);
 
-		// set onclick behavior for permissions list
-		permissionsList.setOnItemClickListener(new OnItemClickListener() {
+			// set onclick behavior for permissions list
+			permissionsList.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				Log.i("PermissionExpectedViewHelper", "clicked permission");
-				// get previously selected permission that need to be displayed
-				Permission permission = (Permission) parent
-						.getItemAtPosition(position);
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					Log.i("PermissionExpectedViewHelper", "clicked permission");
+					// get previously selected permission that need to be
+					// displayed
+					Permission permission = (Permission) parent
+							.getItemAtPosition(position);
 
-				// display permission as alert dialog
-				PrivacyCheckerAlert.callPermissionDialogPermission(permission,
-						view.getContext());
-			}
-		});
+					// display permission as alert dialog
+					PrivacyCheckerAlert.callPermissionDialogPermission(
+							permission, view.getContext());
+				}
+			});
 
-		showMoreButton
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			showMoreButton
+					.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-					@Override
-					public void onCheckedChanged(CompoundButton showMoreButton,
-							boolean isChecked) {
-						ListView listView = (ListView) showMoreButton
-								.getRootView()
-								.findViewById(
-										R.id.app_detail_rate_app_permissions_list);
-						ViewGroup.LayoutParams updatedLayout = listView
-								.getLayoutParams();
-						final float scale = showMoreButton.getRootView()
-								.getContext().getResources()
-								.getDisplayMetrics().density;
-						int pixels = (int) (49 * scale);
+						@Override
+						public void onCheckedChanged(
+								CompoundButton showMoreButton, boolean isChecked) {
+							ListView listView = (ListView) showMoreButton
+									.getRootView()
+									.findViewById(
+											R.id.app_detail_rate_app_permissions_list);
+							ViewGroup.LayoutParams updatedLayout = listView
+									.getLayoutParams();
+							final float scale = showMoreButton.getRootView()
+									.getContext().getResources()
+									.getDisplayMetrics().density;
+							int pixels = (int) (49 * scale);
 
-						if (isChecked) {
-							updatedLayout.height = pixels * listView.getCount();
-							listView.setLayoutParams(updatedLayout);
-						} else {
-							// default: show 4 permissions
-							updatedLayout.height = pixels * 4;
-							listView.setLayoutParams(updatedLayout);
+							if (isChecked) {
+								updatedLayout.height = pixels
+										* listView.getCount();
+								listView.setLayoutParams(updatedLayout);
+							} else {
+								// default: show 4 permissions
+								updatedLayout.height = pixels * 4;
+								listView.setLayoutParams(updatedLayout);
+							}
 						}
-					}
-				});
+					});
+
+		}
 
 		return rowView;
 	}
-
 }
